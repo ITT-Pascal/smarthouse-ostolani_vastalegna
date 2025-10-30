d3feba9882889e6273c6b8c3d50a7db0514d9071
lorenzoostolani <lorenzo.ostolani.stud@ispascalcomandini.it>
2025-10-30 23:02:06 +0100

    Commit 30/10 Completata classe Lamp e relativi test

--- SmartHouse/src/BlaisePascal.SmartHouse.Domain/Lamp.cs
+++ SmartHouse/src/BlaisePascal.SmartHouse.Domain/Lamp.cs
@@ -2,6 +2,31 @@
 {
     public class Lamp
     {
-        public bool isOn {  get; private set; }
+        public bool IsOn {  get; private set; }   
+        public int BrightnessLevel { get; private set; }
+        public int MaxBrightnessLevel { get; private set; } = 100;
+
+        public Lamp() 
+        {
+            IsOn = false;
+        }
+
+        public void TurnOff()
+        {
+            IsOn = false;
+        }
+        public void TurnOn()
+        {
+            IsOn = true;
+        }
+
+        public void SetBrightness(int levelOfBrightness)
+        {
+            if (levelOfBrightness < 0 || levelOfBrightness > MaxBrightnessLevel)
+            {
+                throw new ArgumentOutOfRangeException("Brightness level must be between 0 and MaxBrightnessLevel.");
+            }
+            BrightnessLevel = levelOfBrightness;
+        }
     }
 }
