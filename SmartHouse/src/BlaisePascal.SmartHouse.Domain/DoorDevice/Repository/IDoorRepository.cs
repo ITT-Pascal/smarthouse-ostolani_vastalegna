3bd0da255afd62ccdc516dd7e98336a68a65e294
lorenzo.ostolani <lorenzo.ostolani.stud@ispascalcomandini.it>
2026-02-11 11:01:40 +0100

    Added IRepository interface

---
+++ SmartHouse/src/BlaisePascal.SmartHouse.Domain/DoorDevice/Repository/IDoorRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlaisePascal.SmartHouse.Domain.LuminuosDevice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.DoorDevice.Repository
+{
+    public interface IDoorRepository
+    {
+        void Add(Door lamp);
+        void Update(Door lamp);
+        void Remove(Guid id);
+        Door GetById(Guid id);
+        List<Door> GetAll();
+    }
+}
