25a8cb2ed0109fd55c58a298f7464f8a71a2c57e
lorenzoostolani <lorenzo.ostolani.stud@ispascalcomandini.it>
2026-02-20 09:51:31 +0100

    Added dto implementation

---
+++ SmartHouse/src/BlaisePascal.SmartHouse.Application/Devices/Lightning/Lamps/Dto/LampDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.Devices.Lightning.Lamps.Dto
+{
+    //public record LampDto(Guid Id, string Name, string Status, int Brightness, DateTime CreatedAtUtc, DateTime LastModifiedAtUtc);
+
+    public class LampDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public int Brightness { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime LastModifiedAtUtc { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\n" +
+                   $"Name: {Name}\n" +
+                   $"Status: {Status}\n" +
+                   $"Brightness: {Brightness}\n" +
+                   $"Created: {CreatedAtUtc}\n" +
+                   $"Last update: {LastModifiedAtUtc}\n";
+        }
+    }
+}
---
+++ SmartHouse/src/BlaisePascal.SmartHouse.Application/Devices/Mapper/DeviceStatusMapper.cs
@@ -0,0 +1,32 @@
+﻿using BlaisePascal.SmartHouse.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.Devices.Mapper
+{
+    public class DeviceStatusMapper
+    {
+        public static string ToDto(DeviceStatus status)
+        {
+            return status switch
+            {
+                DeviceStatus.On => "ON",
+                DeviceStatus.Off => "OFF",
+                //add other status
+            };
+        }
+
+        public static DeviceStatus ToDomain(string status)
+        {
+            return status switch
+            {
+                "ON" => DeviceStatus.On,
+                "OFF" => DeviceStatus.Off,
+                _ => throw new ArgumentException("non valid status")
+            };
+        }
+    }
+}
