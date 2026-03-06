42bba800f5606e693eeb65e3ea35c73784c2a17b
MatteoAgostinoVastalegna <matteoagostino.vastalegna.stud@ispascalcomandini.it>
2026-03-06 09:51:21 +0100

    CCTV commands classi create; add, remove e switchon completi

---
+++ SmartHouse/src/BlaisePascal.SmartHouse.Application/Devices/CCTVDevice/Command/AddCCTVCommand.cs
@@ -0,0 +1,27 @@
+﻿using BlaisePascal.SmartHouse.Domain.Abstraction;
+using BlaisePascal.SmartHouse.Domain.CCTVDevice;
+using BlaisePascal.SmartHouse.Domain.CCTVDevice.Repository;
+using BlaisePascal.SmartHouse.Domain.LuminuosDevice;
+using BlaisePascal.SmartHouse.Domain.LuminuosDevice.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.Devices.CCTVDevice.Command
+{
+    public class AddCCTVCommand
+    {
+        private readonly ICCTVRepository _cctvRepository;
+        public AddCCTVCommand(ICCTVRepository repository)
+        {
+            _cctvRepository = repository;
+        }
+        public void Execute(string name)
+        {
+            _cctvRepository.Add(new CCTV(DeviceName.Create(name)));
+        }
+
+    }
+}
---
+++ SmartHouse/src/BlaisePascal.SmartHouse.Application/Devices/CCTVDevice/Command/RecordCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.Devices.CCTVDevice.Command
+{
+    internal class RecordCommand
+    {
+    }
+}
